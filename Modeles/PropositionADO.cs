1c3195da8c3476c07631d588aa82f87eccda9692
1353331 <1353331@cstj.qc.ca>
2020-10-07 14:09:41 -0400

    Propositions/PropositionsADO

---
+++ Modeles/PropositionADO.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeCollectionneur.Outils;
+
+namespace LeCollectionneur.Modeles
+{
+	public class PropositionADO
+	{
+		private BdBase MaBd;
+
+		public PropositionADO()
+		{
+			MaBd = new BdBase();
+		}
+
+		#region CREATE
+
+		public void Ajouter(Proposition prop)
+		{
+			//string requete = $@"
+			//INSERT INTO Propositions (Id, IdAnnonce, IdUtilisateur, Montant, Date, IdEtatProposition)
+			//VALUES
+			//(NULL, {prop.AnnonceLiee.Id}, {prop.Proposeur.Id}, {prop.Montant}, '{prop.DateProposition}', SELECT Id FROM EtatsPropositions WHERE Nom = '{prop.EtatProposition}')
+			//";
+			//MaBd.Commande(requete);
+		}
+
+		#endregion
+
+		#region READ
+
+		public ObservableCollection<Proposition> RecupererTout()
+		{
+			ObservableCollection<Proposition> ocPropositions = new ObservableCollection<Proposition>();
+
+			string requeteProposition = @"
+			SELECT p.Id, p.idAnnonce, p.idUtilisateur, p.montant, p.date AS dateProposition, ep.Nom AS etatProposition
+			FROM Propositions p
+			LEFT JOIN EtatsProposition ep
+			ON p.IdEtatProposition = ep.Id
+			";
+
+			DataSet setProposition = MaBd.Selection(requeteProposition);
+			DataTable TableProposition = setProposition.Tables[0];
+
+			foreach (DataRow drProposition in TableProposition.Rows)
+			{
+				ocPropositions.Add(new Proposition(drProposition));
+			}
+
+			return ocPropositions;
+		}
+
+		public Proposition RecupererUnParId(int idProposition)
+		{
+			string requete = $@"
+			SELECT p.Id, p.idAnnonce, p.idUtilisateur, p.montant, p.date AS dateProposition, ep.Nom AS etatProposition
+			FROM Propositions p
+			LEFT JOIN EtatsProposition ep
+			ON p.IdEtatProposition = ep.Id
+			WHERE p.Id = {idProposition} 
+			";
+
+			DataSet setProposition = MaBd.Selection(requete);
+			DataTable TableProposition = setProposition.Tables[0];
+
+			return new Proposition(TableProposition.Rows[0]);
+		}
+
+		#endregion
+
+		#region UPDATE
+
+		private void Modifier(Proposition prop)
+		{
+			//string requete = $@"
+			//UPDATE Propositions (Id, IdAnnonce, IdUtilisateur, Montant, Date, IdEtatProposition)
+			//SET
+			//IdAnnonce = {prop.AnnonceLiee.Id},
+			//IdUtilisateur = {prop.Proposeur.Id},
+			//Montant = {prop.Montant},
+			//Date = '{prop.DateProposition}',
+			//IdEtatProposition = SELECT Id FROM EtatsPropositions WHERE Nom = '{prop.EtatProposition}'
+			//WHERE Id = {prop.Id};
+			//";
+
+			//MaBd.Commande(requete);
+		}
+
+		#endregion
+
+		#region DELETE
+	  private void Supprimer(int IdProposition)
+	  {
+			//string requete = $@"
+			//DELETE FROM Propositions
+			//WHERE Id = {IdProposition}
+			//";
+		}
+		
+
+		#endregion
+
+	}
+}
---
+++ Vues/UCPropositionsRecuesEnvoyees.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LeCollectionneur.Vues
+{
+	/// <summary>
+	/// Logique d'interaction pour UCProposition.xaml
+	/// </summary>
+	public partial class UCPropositionsRecuesEnvoyees : UserControl
+	{
+		public UCPropositionsRecuesEnvoyees()
+		{
+			InitializeComponent();
+		}
+
+		private void radPropositionsRecues_Checked(object sender, RoutedEventArgs e)
+		{
+			// Vérifier si les contrôles sont initialisés (pour le loading initial de la page)
+			if (btnRefuserProposition != null)
+			{
+				btnRefuserProposition.Visibility = Visibility.Visible;
+				btnAccepterProposition.Visibility = Visibility.Visible;
+				btnMessageProposition.Visibility = Visibility.Visible;
+
+				lblEtatProposition.Visibility = Visibility.Hidden;
+				txbEtatProposition.Visibility = Visibility.Hidden;
+				btnMessageOffre.Visibility = Visibility.Hidden;
+				btnAnnulerProposition.Visibility = Visibility.Hidden;
+			}
+		}
+
+		private void radPropositionsEnvoyees_Checked(object sender, RoutedEventArgs e)
+		{
+			btnRefuserProposition.Visibility = Visibility.Hidden;
+			btnAccepterProposition.Visibility = Visibility.Hidden;
+			btnMessageProposition.Visibility = Visibility.Hidden;
+
+			lblEtatProposition.Visibility = Visibility.Visible;
+			txbEtatProposition.Visibility = Visibility.Visible;
+			btnMessageOffre.Visibility = Visibility.Visible;
+			btnAnnulerProposition.Visibility = Visibility.Visible;
+		}
+
+	}
+}
