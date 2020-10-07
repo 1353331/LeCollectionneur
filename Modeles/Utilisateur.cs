67bf415caf383d319ad02d6d4efc39850f551078
eti246 <desrocherset@hotmail.com>
2020-10-07 13:51:10 -0400

    Tache 1.4 ED

---
+++ Modeles/Utilisateur.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeCollectionneur.Modeles
+{
+    class Utilisateur
+    {
+        #region Variable
+        //Variable nécésaire pour le fonctionnement de l'application
+        public int Id { get; set; }
+        public string NomUtilisateur { get; set; }
+        public string Courriel { get; set; }
+        #endregion
+
+        #region Constructeur
+        //Constructeur Complet 
+        public Utilisateur(int Id, string NomUtilisateur,string Courriel)
+        {
+            //Assignation des Champs
+            this.Id = Id;
+            this.NomUtilisateur = NomUtilisateur;
+            this.Courriel = Courriel;
+        }
+        //Contrusteur par DataSet
+        public Utilisateur(DataSet Compte)
+        {
+            this.Id = (int)Compte.Tables[0].Rows[0]["id"];
+            this.NomUtilisateur = Compte.Tables[0].Rows[0]["NomUtilisateur"].ToString();
+            this.Courriel = Compte.Tables[0].Rows[0]["Courriel"].ToString();
+        }
+        //Constructeur Vide pour Utilisateur Ado
+        public Utilisateur(){ }
+        #endregion
+    }
+}
---
+++ Modeles/UtilisateurADO.cs
@@ -0,0 +1,68 @@
+﻿using LeCollectionneur.Outil;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LeCollectionneur.Modeles
+{
+    class UtilisateurADO
+    {
+        public BDbase BD = new BDbase();
+        #region Variable
+        Utilisateur utilisateur;
+
+        #endregion
+
+        #region Constructeur
+        //Constructeur Vide
+        public UtilisateurADO() { }
+        #endregion
+
+        #region Method
+        //Method de validation des champs entré par L'utilisateur
+        public bool InfoValideConnection(string User, string MP)
+        {
+            //Va chercher le compte utlisant le User
+            DataSet compte = GetUser(User);
+            //Valide que le user existe dans la bd
+            if (compte.Tables[0].Rows.Count == 0)
+                //Si la fonction a GetUser n'a pas pu retourner d'info alors le compte n'existe pas
+                return false;
+
+            if (compte.Tables[0].Rows[0]["MotDePasse"].ToString() != MP)
+                // Si le mot de passe n'est pas valide on retourne false
+                return false;
+            //Les donnée saisie sont valide 
+            return true;
+        }
+
+        //Method qui retourne un compte par le User
+        private DataSet GetUser(string User)
+        {
+            string req = "SELECT * FROM `utilisateurs` WHERE NomUtilisateur = '" + User + "'";
+            return BD.Selection(req);
+        }
+
+        //Connecte L'utilisateur rentre ces informations dans un objet Utilisateur
+        public void Connection(string User,string MP)
+        {
+            if(InfoValideConnection(User,MP))
+            {
+                utilisateur = new Utilisateur(GetUser(User)); 
+            }
+        }
+
+        //Créé un compte en BD
+        public void CreerCompte()
+        {
+
+        }
+
+
+        #endregion
+    }
+}
