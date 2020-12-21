de897c61c56f70dac8d2aa0e598078286a5f3966
1353331 <1353331@cstj.qc.ca>
2020-12-21 10:59:19 -0500

    merge encore

---
+++ Migrations/202012211547429_V2.cs
@@ -0,0 +1,18 @@
+﻿namespace LeCollectionneur.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class V2 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Utilisateurs", "DarkMode", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Utilisateurs", "DarkMode");
+        }
+    }
+}
